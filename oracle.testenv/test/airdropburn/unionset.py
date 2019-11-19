ede5e0ec95bd15d4f20b95066f2b87754403c950
vlbos <vlbos2018@gmail.com>
2019-11-19 21:19:21 +0800

    update msig python script

--- oracle.testenv/test/airdropburn/unionset.py
+++ oracle.testenv/test/airdropburn/unionset.py
@@ -1,11 +1,14 @@
-#coding:utf-8
+# coding:utf-8
 import csv
 import re
-#txt
+import json
+# txt
+
+
 def loadtxt(txt):
-    f=open(txt, 'r')
-    sourceInline=f.readlines()
-    dataset=[]
+    f = open(txt, 'r')
+    sourceInline = f.readlines()
+    dataset = []
     for line in sourceInline:
         temp1 = line.strip('\x00')
         temp1 = temp1.strip('\n')
@@ -16,6 +19,8 @@ def loadtxt(txt):
     return dataset
 
 # csv
+
+
 def loadcsv(csvFile):
     f = open(csvFile, 'r')
     reader = csv.reader(f)
@@ -23,14 +28,15 @@ def loadcsv(csvFile):
     csvlist = []
     for item in reader:
         csvset[item[0]] = item[1]
-        csvlist.append(item[0])    
+        csvlist.append(item[0])
     f.close()
     return csvset, csvlist
+
 
 def unionset():
     # 读取txt获取插件账户
     txt = './dataset/nonactivated_bos_accounts.txt'
-    tacclist=loadtxt(txt)
+    tacclist = loadtxt(txt)
 
     # 读取csv集合
     csvFile = './dataset/airdrop_accounts.csv'
@@ -39,43 +45,63 @@ def unionset():
     # 创建数值集合
     tc = set(tacclist) & set(cacclist)
 
-    resCsv=[]
-    sumBurn=0
+    resCsv = []
+    sumBurn = 0
     # 构造交集结果
     for item in tc:
-        resCsv.append([item,caccset[item]])
-        sumBurn += float(caccset[item].replace('BOS','').strip())
+        resCsv.append([item, caccset[item]])
+        sumBurn += float(caccset[item].replace('BOS', '').strip())
 
-    print('len of unactive',len(resCsv))
-    msig,sumBurnmsig=intersectmsigset()
+    print('len of unactive', len(resCsv))
+    msig, sumBurnmsig = intersectmsigset()
     resCsv += msig
 
     # 写结果
-    with open('./airdrop_unactive_account.csv','w') as cfile:
+    with open('./airdrop_unactive_account.csv', 'w') as cfile:
         writer = csv.writer(cfile)
         for item in resCsv:
             writer.writerow(item)
-        
+
     cfile.close()
-    
-    print('len of unactive include msig',len(resCsv))
-    print('sumBurn inclue msig',sumBurn+sumBurnmsig)
+
+    print('len of unactive include msig', len(resCsv))
+    print('sumBurn inclue msig', (sumBurn+sumBurnmsig))
+
+
+def msigfromjson():
+    # 由于文件中有多行，直接读取会出现错误，因此一行一行读取
+    file = open("/Users/lisheng/Downloads/burn/bos-airdrop-snapshots-master/accounts_info_bos_snapshot.airdrop.msig.json", 'r', encoding='utf-8')
+    papers = []
+    for line in file.readlines():
+        dic = json.loads(line)
+        print(dic)
+        papers.append(dic)
+
+
+    print(len(papers))
+    print(papers)
+    # print(papers['bos_account'])
+    # print(papers['bos_balance'])
+    for key in papers:
+        print(key['bos_account'], key['bos_balance'])
 
 def readmsigfromlog():
     txt = './dataset/seq.log'
-    f=open(txt, 'r')
-    sourceInline=f.read()
+    f = open(txt, 'r')
+    sourceInline = f.read()
     #'get_unused_accounts  ] ----- rncsqdohedjq -- auth_sequence: 0 -----'
     'get_unused_accounts  ] ----- (.*?) -- auth_sequence: (\d+) -----'
-    pattern = re.compile('get_unused_accounts  ] ----- (.*?) -- auth_sequence: (\d+) -----', re.S)
+    pattern = re.compile(
+        'get_unused_accounts  ] ----- (.*?) -- auth_sequence: (\d+) -----', re.S)
     # compile可以在多次使用中提高效率，这里影响不大
     results = re.findall(pattern, sourceInline)
     accseqmap = {}
     for result in results:
         acc, nums = result
-        accseqmap[acc]=nums
+        accseqmap[acc] = nums
         # print(acc,  nums)
     return accseqmap
+
 
 def intersectmsigset():
     # 读取csv集合
@@ -87,16 +113,16 @@ def intersectmsigset():
     # 创建数值集合
     # tc = set(tacclist) & set(cacclist)
 
-    resCsv=[]
-    acacc=[]
-    sumBurn=0
+    resCsv = []
+    acacc = []
+    sumBurn = 0
     # 构造交集结果
     for item in msiglist:
-        if int(accseq[item])==2:
-            resCsv.append([item,msigmap[item]])
-            sumBurn += float(msigmap[item].replace('BOS','').strip())
+        if int(accseq[item]) == 2:
+            resCsv.append([item, msigmap[item]])
+            sumBurn += float(msigmap[item].replace('BOS', '').strip())
         else:
-            acacc.append([item,msigmap[item],accseq[item]])
+            acacc.append([item, msigmap[item], accseq[item]])
 
     # # 写结果
     # with open('./output/msig_unactive_acc.csv','w') as cfile:
@@ -111,9 +137,10 @@ def intersectmsigset():
     #         writer.writerow(item)
     # mcfile.close()
 
-    print('len unactive',len(resCsv))
-    print('len active',len(acacc))
-    print('msumBurn',sumBurn)
-    return resCsv,sumBurn
+    print('len unactive', len(resCsv))
+    print('len active', len(acacc))
+    print('msumBurn', sumBurn)
+    return resCsv, sumBurn
 
-unionset()
+
+msigfromjson()

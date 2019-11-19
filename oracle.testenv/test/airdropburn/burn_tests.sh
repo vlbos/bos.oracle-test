94e11ebc323f75e4c144154f26cc4650aac93f96
vlbos <vlbos2018@gmail.com>
2019-11-19 17:34:35 +0800

    add msig csv

--- oracle.testenv/test/airdropburn/burn_tests.sh
+++ oracle.testenv/test/airdropburn/burn_tests.sh
@@ -1,166 +1,84 @@
 #!/usr/bin/env bash
 
-. env.sh
-#. chains_init.sh
+contract_repo_dir=bos.contracts
 
-set_contracts() {
-    cleos=cleos1 && if [ "$1" == "c2" ]; then cleos=cleos2; fi
-    ${!cleos} set contract ${contract_burn} ${CONTRACTS_DIR}/${contract_burn_folder} -x 1000 -p ${contract_burn}
-    sleep .2
-    
-    ${!cleos} set account permission ${contract_burn} active '{"threshold": 1,"keys": [{"key": "'${burn_c_pubkey}'","weight": 1}],"accounts": [{"permission":{"actor":"'${contract_burn}'","permission":"eosio.code"},"weight":1}]}' owner -p ${contract_burn}@owner
+# CONTRACTS_DIR=~/${contract_repo_dir}/build/contracts/
+CONTRACTS_DIR=~/mygit/vlbos/burn/${contract_repo_dir}/build/contracts/
+# CONTRACTS_DIR=~/abos/${contract_repo_dir}
+WALLET_DIR=~/eosio-wallet
+
+http_endpoint='http://127.0.0.1:8888'
+cleos1="cleos -u "$http_endpoint
+
+contract_burn=burn.bos
+contract_burn_folder=bos.burn
+
+
+burn_c_pubkey=EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV
+burn_c_prikey=5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3
+
+
+
+start_keosd(){
+    killall keosd 2>/dev/null
+    # rm -rf ${WALLET_DIR}
+    mkdir ${WALLET_DIR}
+    nohup keosd --wallet-dir ${WALLET_DIR} --unlock-timeout 90000 1>/dev/null  2>/dev/null &
+}
+start_keosd
+
+
+create_wallet(){
+    # rm -rf ~/eosio-wallet/
+    cleos wallet create -f ${WALLET_DIR}/password.txt
+    cleos wallet import --private-key 5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3
+}
+create_wallet
+
+import_key(){
+    echo prikey=$1
+    cleos wallet import --private-key $1
 }
 
-test_set_contracts() {
-    set_contracts c1
-    # set_contracts c2
-}
-
-get_account() {
-    echo --- cleos1 --- $1
-    $cleos1 get account $1
-    # echo && echo --- cleos2 ---  $1
-    # $cleos2 get account  $1
-}
-
-test_get_account() {
-    get_account "$1"
-    # get_account oraclize1111
-    # get_account bosbosburn
-    # get_account ${contract_burn}
-}
-
-transfer() {
-    echo --- cleos1 transfer ---
-    $cleos1 transfer provider1111 ${contract_burn} "0.0001 BOS" "0,0" -p provider1111
-    $cleos1 transfer consumer2222 ${contract_burn} "0.0001 BOS" "1,0" -p consumer2222
-    $cleos1 transfer consumer2222 ${contract_burn} "0.0001 BOS" "2,consumer2222,consumer1111,0" -p consumer2222
-    $cleos1 transfer consumer2222 ${contract_burn} "0.0001 BOS" "3,0" -p consumer2222
-    
-    # $cleos2 transfer  testblklist1 testblklist2 "10.0000 BOS" "ibc receiver=chengsong111" -p testblklist1
-    #
-}
-
-# dataservices
-# servicefees
-# providers
-# provservices
-# svcprovision
-# cancelapplys
-
-# pushrecords
-# ppushrecords
-# apushrecords
-# papushrecord
-
-# provisionlog
-transfer0() {
-    echo --- providers before transfer ---
-    test_get_table providers
-    echo --- svcprovision before transfer ---
-    test_get_table1 0 svcprovision
-    echo --- servicestake before transfer ---
-    test_get_table servicestake
-    echo --- cleos1 transfer service stake---
-    $cleos1 transfer provider1111 ${contract_burn} "0.0001 BOS" "0,0" -p provider1111
-    echo --- providers after transfer ---
-    test_get_table providers
-    echo --- svcprovision after transfer ---
-    test_get_table1 0 svcprovision
-    echo --- servicestake after transfer ---
-    test_get_table servicestake
-}
-
-pwd = 'cat ~/eosio-wallet/password.txt'
-
-test_list_pri_key() {
-    cleos=cleos1 && if [ "$1" == "c2" ]; then cleos=cleos2; fi
-    ${!cleos} wallet private_keys --password PW5JhG3FdGXSc2RTXRNC2tDrd4KhudMA1BggF9QvxJ6YUL4ktUh4k
-}
+import_key ${burn_c_prikey}
 
 get_burn_table() {
-    cleos=cleos1 && if [ "$1" == "c2" ]; then cleos=cleos2; fi
-    echo $2
-    ${!cleos} get table ${contract_burn} ${contract_burn} $2 --limit 100
-    
-}
-get_burn_table1() {
-    cleos=cleos1 && if [ "$1" == "c2" ]; then cleos=cleos2; fi
-    
-    ${!cleos} get table ${contract_burn} $2 $3 --limit 100
+    ${!cleos1} get table ${contract_burn} ${contract_burn} $1 --limit 100
     
 }
 
-test_get_table() {
-    get_burn_table c1 $1
-}
-
-test_get_table1() {
-    get_burn_table1 c1 $1 $2
+get_burn_table_scope() {
+    ${cleos1} get table ${contract_burn} $1 $2 --limit 100
 }
 
 get_info() {
-    cleos=cleos1 && if [ "$1" == "c2" ]; then cleos=cleos2; fi
-    
-    #cleos get table ${contract_token} ${contract_token} globals
-    ${!cleos} get info
-    
+    ${!cleos1} get info
 }
 get_scope() {
-    cleos=cleos1 && if [ "$1" == "c2" ]; then cleos=cleos2; fi
+    ${!cleos1} get scope -t stat eosio.token
+    ${!cleos1} get scope -t accounts ${contract_burn}
+}
+
+set_contracts() {
+    ${cleos1} set contract ${contract_burn} ${CONTRACTS_DIR}/${contract_burn_folder} -x 1000 -p ${contract_burn}
+    sleep .2
     
-    #cleos get table ${contract_token} ${contract_token} globals
-    ${!cleos} get scope -t stat eosio.token
-    ${!cleos} get scope -t accounts ${contract_burn}
-    
+    ${cleos1} set account permission ${contract_burn} active '{"threshold": 1,"keys": [{"key": "'${burn_c_pubkey}'","weight": 1}],"accounts": [{"permission":{"actor":"'${contract_burn}'","permission":"eosio.code"},"weight":1}]}' owner -p ${contract_burn}@owner
 }
 
-test_get_scope() {
-    get_scope c1
-}
 
-test_get_info() {
-    get_info c1
-}
 
-test_importaccounts() {
-    # $cleos1 push action ${contract_burn} importacnts '[[{"account":"provider3333", "quantity":"0.0001 BOS"},{"account":"provider2222", "quantity":"0.0001 BOS"}]]' -p ${contract_burn}
-    ${cleos1} push action ${contract_burn} importacnts '[[["provider3333","0.0001 BOS"],["provider4444","0.0001 BOS"]]]' -p ${contract_burn}
-    
-    test_get_table1 provider3333 accounts
-    
-}
-
-test_clear() {
-    ${cleos1} push action ${contract_burn} clear '[["provider3333","provider4444"]]' -p ${contract_burn}
-}
-
-test_setparameter() {
+setparameter() {
     # ${cleos1} push action ${contract_burn} setparameter '[1,"'${contract_burn}'"]' -p ${contract_burn}
-    outputresult=$(${cleos1} push action ${contract_burn} setparameter '[1,"'${contract_burn}'"]' -p ${contract_burn})
-    
-    substringindex  $outputresult  "2"
+    ${cleos1} push action ${contract_burn} setparameter '[1,"'${contract_burn}'"]' -p ${contract_burn}
+
     
 }
 
-test_burn() {
+burn_from_hole() {
     ${cleos1} push action ${contract_burn} burn '["0.0001 BOS"]' -p ${contract_burn}
 }
 
-test_trnasferair() {
-    ${cleos1} push action ${contract_burn} transferair '["vsyqzsaynmqq"]' -p provider4444
-    test_get_table1 provider3333 accounts
-}
-
-test_checkresult() {
-    result=$($cleos1 get account provider1111)
-    echo $result
-    if [[ "$string" =~ "abc" ]] ; then
-        echo MATCH !
-    else
-        echo NO MATCH !
-    fi
-}
 
 flag=0
 count=0
@@ -171,19 +89,18 @@ file=./airdropburn/airdrop_unactive_account.csv
 test_() {
     OLD_IFS=$IFS #保存原始值
     IFS="="
-    # cleos=$(echo $cleos1 | tr -d '\r')
-    s="http://127.0.0.1:8888"
-    cleos -u $s push action ${contract_burn} importacnts '[[["provider3333","0.0001 BOS"]]]' -p ${contract_burn}
+    cleos -u $http_endpoint push action ${contract_burn} importacnts '[[["provider3333","0.0001 BOS"]]]' -p ${contract_burn}
     IFS=$OLD_IFS #还原IFS的原始值
 }
 
-test_importaccs() {
+import_accs() {
     OLD_IFS=$IFS #保存原始值
     IFS="="
-    cleos -u http://127.0.0.1:8888 push action ${contract_burn} importacnts '[['$accs']]' -p ${contract_burn}
+    cleos -u $http_endpoint push action ${contract_burn} importacnts '[['$accs']]' -p ${contract_burn}
     IFS=$OLD_IFS #还原IFS的原始值
 }
-test_csvimport() {
+
+import_from_csv() {
     Start=$(date +%s)
     
     OLD_IFS=$IFS #保存原始值
@@ -194,7 +111,7 @@ test_csvimport() {
         accs=$accs$(echo '["'$name'","'$quantityx'"]')
         count=$(($count + 1))
         if (($(($count % $limits)) == 0)); then
-            test_importaccs
+            import_accs
             echo "=========count========"$count
             End=$(date +%s)
             echo $End
@@ -208,7 +125,7 @@ test_csvimport() {
     
     if (($(($count % $limits)) > 0)); then
         accs=${accs%","}
-        test_importaccs
+        import_accs
         echo "=========count========"$count
     fi
     End=$(date +%s)
@@ -216,58 +133,20 @@ test_csvimport() {
     echo $Time"==============csv end============="
 }
 
-test_arrclear() {
+transferairs() {
     OLD_IFS=$IFS #保存原始值
     IFS="="
-    cleos -u http://127.0.0.1:8888 push action ${contract_burn} clear '[['$accs']]' -p ${contract_burn}
-    IFS=$OLD_IFS #还原IFS的原始值
-}
-test_clearfromcsv() {
-    Start=$(date +%s)
-    
-    OLD_IFS=$IFS #保存原始值
-    IFS=","
-    while read name quantity; do
-        accs=$accs'"'$name'"'
-        count=$(($count + 1))
-        if (($(($count % $limits)) == 0)); then
-            test_arrclear
-            echo "=========count========"$count
-            End=$(date +%s)
-            echo $End
-            accs=''
-        else
-            accs=$accs','
-        fi
-    done <$file
-    
-    IFS=$OLD_IFS #还原IFS的原始值
-    
-    if (($(($count % $limits)) > 0)); then
-        accs=${accs%","}
-        est_arrclear
-        echo "=========count========"$count
-    fi
-    End=$(date +%s)
-    Time=$(($End-$Start))
-    
-    echo $Time"==============clear end============="
-}
-
-test_transferairs() {
-    OLD_IFS=$IFS #保存原始值
-    IFS="="
-    cleos -u http://127.0.0.1:8888 push action ${contract_burn} transferairs '["'$1'"]' -p ${contract_burn}
+    cleos -u $http_endpoint push action ${contract_burn} transferairs '["'$1'"]' -p ${contract_burn}
     IFS=$OLD_IFS #还原IFS的原始值
 }
 
-test_csvtransferairs() {
+transferairs_from_csv() {
     Start=$(date +%s)
     OLD_IFS=$IFS #保存原始值
     IFS=","
     count=0
     while read name quantity; do
-        test_transferairs $name
+        transferairs $name
         End=$(date +%s)
         count=$(($count + 1))
         echo $count"=====transferairs==Time===="
@@ -280,24 +159,68 @@ test_csvtransferairs() {
     echo $Time"==============airs end======Time======="
 }
 
+arr_clear() {
+    OLD_IFS=$IFS #保存原始值
+    IFS="="
+    cleos -u $http_endpoint push action ${contract_burn} clear '[['$accs']]' -p ${contract_burn}
+    IFS=$OLD_IFS #还原IFS的原始值
+}
+clear_from_csv() {
+    Start=$(date +%s)
+    
+    OLD_IFS=$IFS #保存原始值
+    IFS=","
+    while read name quantity; do
+        accs=$accs'"'$name'"'
+        count=$(($count + 1))
+        if (($(($count % $limits)) == 0)); then
+            arr_clear
+            echo "=========count========"$count
+            End=$(date +%s)
+            echo $End
+            accs=''
+        else
+            accs=$accs','
+        fi
+    done <$file
+    
+    IFS=$OLD_IFS #还原IFS的原始值
+    
+    if (($(($count % $limits)) > 0)); then
+        accs=${accs%","}
+        arr_clear
+        echo "=========count========"$count
+    fi
+    End=$(date +%s)
+    Time=$(($End-$Start))
+    
+    echo $Time"==============clear end============="
+}
+
+checkresult() {
+    result=$($cleos1 get account provider1111)
+    echo $result
+    if [[ "$string" =~ "abc" ]] ; then
+        echo MATCH !
+    else
+        echo NO MATCH !
+    fi
+}
+
 
 case "$1" in
+    "set") set_contracts ;;
+    "imp") import_from_csv ;;
+    "setp") setparameter ;;
+    "ct") transferairs_from_csv ;;
+    "chk") checkresult ;;
+    "burn") burn_from_hole ;;
+    "clr") clear_from_csv ;;
+    "acc") get_account "$2" ;;
+    "table") get_burn_table "$2" ;;
+    "tables") get_burn_table_scope "$2" "$3" ;;
+    "info") get_info ;;
+    "scope") get_scope ;;
     "test") test_ "$2";;
-    "chk") test_checkresult ;;
-    "imp") test_importaccounts "$2" ;;
-    "clear") test_clear ;;
-    "setp") test_setparameter ;;
-    "burn") test_burn ;;
-    "air") test_trnasferair;;
-    "ct") test_csvtransferairs ;;
-    "ci") test_csvimport ;;
-    "set") test_set_contracts ;;
-    "acc") test_get_account "$2" ;;
-    "transfer") test_transfer "$2" ;;
-    "keys") test_list_pri_key ;;
-    "table") test_get_table "$2" ;;
-    "table1") test_get_table1 "$2" "$3" ;;
-    "info") test_get_info ;;
-    "scope") test_get_scope ;;
-    *) echo "usage: burn_test.sh chk|imp|clear|setp|burn|air|ct|ci|set|acc|transfer|keys|table {name}|table1 {scope name}|info|scope" ;;
+    *) echo "usage: burn_test.sh set|imp|chk|clr|setp|burn|air|ct|ci|acc|transfer|keys|table {name}|tables {scope name}|info|scope" ;;
 esac
